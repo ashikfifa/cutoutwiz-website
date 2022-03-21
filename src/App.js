38a0d354e560bea31fe5d1c6ccc94ac5d3d63d35
ashikfifa <ashikahmedfifa@gmail.com>
2022-03-21 14:03:03 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Home, Contact, Blog, Postphotography, Footer} from "./components";
+import Typesofphoto from "./components/typesofphoto";
+import Wintermarketing from "./components/Wintermarketing";
+import Easyproductphoto from "./components/Easyproductphoto";
+import Topshopify from "./components/Topshopify";
+import Videoediting from "./components/Videoediting";
+import Softbox from "./components/Softbox";
+import Minimizebounce from "./components/Minimizebounce";
+import Termsconditions from "./components/Termsconditions";
+import Privacypolicy from "./components/Privacypolicy";
+import ClothingPhoto from "./components/ClothingPhoto";
+import CarPhoto from "./components/CarPhoto";
+import ProductReselling from "./components/ProductReselling";
+import FoodPhoto from "./components/FoodPhoto";
+import InfluenceMarketing from "./components/InfluenceMarketing";
+import ScrollToTop from "./components/ScrollToTop";
+import Newyear from "./components/Newyear";
+import Productphotography from "./components/Productphotography";
+import Thankyou from "./components/Thankyou";
+import Demoblog from "./components/Demo-blog";
+import NotFound from "./components/NotFound";
+import AllBlogs from "./components/AllBlogs";
+import Bookacall from "./components/Bookacall"
+import Contacttest from "./components/Contacttest";
+import Shoptalk from "./components/Shoptalk";
+import Shoptalk2 from "./components/Shoptalk2";
+import Shoptalk3 from "./components/Shoptalk3";
+function App() {
+
+  return (
+    <Router >
+    <div className="App">
+    <ScrollToTop/>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/contact-us" exact component={() => <Contact />} />
+          <Route path="/blog" exact component={() => <Blog />} />
+          <Route path="/footer" exact component={() => <Footer />} />
+          <Route path="/5-important-photography-tips-for-beginners" exact component={() => <Postphotography />} />
+          <Route path="/types-of-photography-which-one-drive-your-inner-passion-most" exact component={() => <Typesofphoto />} />
+          <Route path="/7-creative-winter-marketing-ideas-for-e-commerce-businesses" exact component={() => <Wintermarketing />} />
+          <Route path="/easy-product-photography-solutions-for-small-business" exact component={() => <Easyproductphoto />} />
+          <Route path="/top-shopify-marketing-strategy-for-sellers" exact component={() => <Topshopify />} />
+          <Route path="/5-interesting-video-editing-facts" exact component={() => <Videoediting />} /> 
+          <Route path="/minimize-your-bounce-rate" exact component={() => <Minimizebounce />} /> 
+          <Route path="/how-to-make-a-softbox-for-product-photography" exact component={() => <Softbox />} /> 
+          <Route path="/terms-and-conditions" exact component={() => <Termsconditions />} /> 
+          <Route path="/privacy-policy" exact component={() => <Privacypolicy />} /> 
+          <Route path="/top-new-year-resolution-for-your-online-store" exact component={() => <Newyear />} /> 
+          <Route path="/product-photography-how-to-take-photos-for-your-e-commerce-business" exact component={() => <Productphotography />} /> 
+          <Route path="/a-beginners-how-to-guide-for-clothing-photography" exact component={() => <ClothingPhoto />} /> 
+          <Route path="/car-photography-a-beginners-how-to-guide-and-tips" exact component={() => <CarPhoto />} /> 
+          <Route path="/product-reselling-photography-why-and-how-to-do-it" exact component={() => <ProductReselling />} /> 
+          <Route path="/top-8-food-photography-tips-for-beginners" exact component={() => <FoodPhoto />} /> 
+          <Route path="/influencer-marketing-how-to-find-the-right-one" exact component={() => <InfluenceMarketing />} /> 
+          <Route path="/thank-you" exact component={() => <Thankyou />} /> 
+          <Route path="/all-blogs" exact component={() => <AllBlogs />} /> 
+          <Route path="/demo-blog" exact component={() => <Demoblog />} /> 
+          <Route path="/contacttest" exact component={() =><Contacttest />}/>
+          <Route path="/book-a-call/" exact component={() => <Bookacall />} /> 
+          <Route path="/shoptalk_2022_a-complete-guide-for-attendees_test" exact component={() => <Shoptalk />} /> 
+          <Route path="/shoptalk_2022_a-complete-guide-for-attendees" exact component={() => <Shoptalk2 />} /> 
+          <Route path="/shoptalk_2022_a-complete-guide-for-attendees/confirmation" exact component={() => <Shoptalk3 />} /> 
+          <Route path="" exact component={NotFound} />
+        </Switch>
+        {/* <Footer/> */}
+    </div>
+    </Router>
+  );
+}
+
+export default App;
